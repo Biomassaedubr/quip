851a963b37e10c2db55cac6a2aedf31d7112a998
Josh Petrie <josh@joshpetrie.net>
2015-10-23 22:31:13 -0700

    Support mouse-based positioning of the cursor.

--- Source/Mac/DocumentView.cs
+++ Source/Mac/DocumentView.cs
@@ -88,18 +88,29 @@ namespace Quip {
       }
     }
 
+    public override void MouseDown (NSEvent eventData) {
+      m_mouseDragOrigin = GetLocationFromCoordinate(eventData.LocationInWindow);
+    }
+
+    public override void MouseUp (NSEvent eventData) {
+      var location = GetLocationFromCoordinate(eventData.LocationInWindow);
+      if (location == m_mouseDragOrigin) {
+        MoveTo(location);
+      }
+    }
+
     public override void DrawRect (RectangleF dirtyRect) {
       base.DrawRect(dirtyRect);
 
       var context = NSGraphicsContext.CurrentContext.GraphicsPort;
-      using (var font = new CTFont("Menlo", 13.0f)) {
+      using (var font = new CTFont(m_fontName, m_fontSize)) {
         var queryDictionary = new NSDictionary(CTStringAttributeKey.Font, font);
         var queryString = new NSString("m");
-        var cellSize = queryString.StringSize(queryDictionary);
+        m_textCellSize = queryString.StringSize(queryDictionary);
 
         var dictionary = new NSDictionary(CTStringAttributeKey.Font, font);
         var attributes = new CTStringAttributes(dictionary);
-        var y = Frame.Height - cellSize.Height;
+        var y = Frame.Height - m_textCellSize.Height;
         for (var index = 0; index < Document.Rows; ++index) {
           var line = Document.GetRow(index);
           var attributed = new NSAttributedString(line, attributes);
@@ -107,7 +118,7 @@ namespace Quip {
             context.TextPosition = new PointF(0.0f, y);
 
             drawable.Draw(context);
-            y -= cellSize.Height;
+            y -= m_textCellSize.Height;
           }
         }
       
@@ -118,29 +129,35 @@ namespace Quip {
         }
 
         foreach (var selection in Selections.All) {
-          DrawSelection(selection, context, cellSize.Width, cellSize.Height);
+          DrawSelection(selection, context);
         }
 
         if (m_cursorIsVisible) {
-          var cursorX = Cursor.Column * cellSize.Width;
-          var cursorY = Frame.Height - cellSize.Height - (Cursor.Row * cellSize.Height);
+          var cursorX = Cursor.Column * m_textCellSize.Width;
+          var cursorY = Frame.Height - m_textCellSize.Height - (Cursor.Row * m_textCellSize.Height);
           var style = m_cursorStack.Count == 0 ? CursorStyle.Underbar : m_cursorStack.Peek();
 
           context.SetFillColor(0.0f, 0.0f, 0.0f, 1.0f);
           switch (style) {
             case CursorStyle.VerticalBar:
-              context.FillRect(new RectangleF(cursorX, cursorY - 2, 1, cellSize.Height - 2));
+              context.FillRect(new RectangleF(cursorX, cursorY - 2, 1, m_textCellSize.Height - 2));
               break;
             case CursorStyle.Underbar:
             default:
-              context.FillRect(new RectangleF(cursorX, cursorY - 2, cellSize.Width, 1));
+              context.FillRect(new RectangleF(cursorX, cursorY - 2, m_textCellSize.Width, 1));
               break;
           }
         }
       }
     }
 
-    void DrawSelection(Selection selection, CGContext context, float cellWidth, float cellHeight) {
+    Location GetLocationFromCoordinate (PointF coordinate) {
+      var column = coordinate.X / m_textCellSize.Width;
+      var row = (Frame.Height - coordinate.Y) / m_textCellSize.Height;
+      return new Location((int)column, (int)row);
+    }
+
+    void DrawSelection(Selection selection, CGContext context) {
       var earlier = selection.Origin < selection.Extent ? selection.Origin : selection.Extent;
       var later = selection.Origin < selection.Extent ? selection.Extent : selection.Origin;
       var row = earlier.Row;
@@ -149,11 +166,11 @@ namespace Quip {
         var firstColumn = row == earlier.Row ? earlier.Column : 0;
         var lastColumn = row == later.Row ? later.Column : Document.GetRow(row).Length - 1;
 
-        var x = firstColumn * cellWidth;
-        var y = Frame.Height - cellHeight - (row * cellHeight);
+        var x = firstColumn * m_textCellSize.Width;
+        var y = Frame.Height - m_textCellSize.Height - (row * m_textCellSize.Height);
 
         context.SetFillColor(1.0f, 0.0f, 0.0f, 1.0f);
-        context.FillRect(new RectangleF(x, y - 1, cellWidth * (lastColumn + 1 - firstColumn), 1));
+        context.FillRect(new RectangleF(x, y - 1, m_textCellSize.Width * (lastColumn + 1 - firstColumn), 1));
 
         ++row;
       } while (row <= later.Row);
@@ -174,8 +191,14 @@ namespace Quip {
     NSTimer m_cursorTimer;
     const double m_cursorBlinkInterval = 0.57;
 
+    Location m_mouseDragOrigin;
+
     const int m_enterKeyCode = 36;
     const int m_backspaceKeyCode = 51;
     const int m_escapeKeyCode = 53;
+
+    SizeF m_textCellSize;
+    const string m_fontName = "Menlo";
+    const float m_fontSize = 13.0f;
   }
 }
