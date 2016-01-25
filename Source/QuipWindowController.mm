c230de6a7d958f69cfd0f6659db2a79c128b347e
Josh Petrie <josh@joshpetrie.net>
2016-01-24 16:42:25 -0800

    Correct find-replace mistakes.

--- Source/QuipWindowController.mm
+++ Source/QuipWindowController.mm
@@ -7,7 +7,7 @@
 @interface QuipWindowController () {
 @private
   NSView * m_rootView;
-  QuipStatusView * m_QuipStatusView;
+  QuipStatusView * m_statusView;
   NSScrollView * m_scrollView;
   QuipTextView * m_documentView;
 }
@@ -15,7 +15,7 @@
 @end
 
 namespace {
-  static CGFloat gQuipStatusViewHeight = 20.0;
+  static CGFloat gStatusViewHeight = 20.0;
 }
 
 @implementation QuipWindowController
@@ -26,13 +26,13 @@ namespace {
   CGRect rootFrame = [[self window] contentRectForFrameRect:[[self window] frame]];
   m_rootView = [[NSView alloc] initWithFrame:rootFrame];
   
-  CGRect statusFrame = CGRectMake(0.0, 0.0, rootFrame.size.width, gQuipStatusViewHeight);
-  m_QuipStatusView = [[QuipStatusView alloc] initWithFrame:statusFrame];
+  CGRect statusFrame = CGRectMake(0.0, 0.0, rootFrame.size.width, gStatusViewHeight);
+  m_statusView = [[QuipStatusView alloc] initWithFrame:statusFrame];
   
-  CGRect contentFrame = CGRectMake(0.0, gQuipStatusViewHeight, rootFrame.size.width, rootFrame.size.height - gQuipStatusViewHeight);
+  CGRect contentFrame = CGRectMake(0.0, gStatusViewHeight, rootFrame.size.width, rootFrame.size.height - gStatusViewHeight);
   
   QuipDocument * document = [self document];
-  m_documentView = [[QuipTextView alloc] initWithFrame:contentFrame document:[document document] status:m_QuipStatusView];
+  m_documentView = [[QuipTextView alloc] initWithFrame:contentFrame document:[document document] status:m_statusView];
   m_scrollView = [[NSScrollView alloc] initWithFrame:contentFrame];
   [m_scrollView setHasHorizontalScroller:YES];
   [m_scrollView setHasVerticalScroller:YES];
@@ -42,7 +42,7 @@ namespace {
   [m_scrollView.contentView scrollToPoint:CGPointMake(0.0, NSMaxY(m_documentView.frame) - NSHeight(m_scrollView.contentView.bounds))];
   
   [m_rootView addSubview:m_scrollView];
-  [m_rootView addSubview:m_QuipStatusView];
+  [m_rootView addSubview:m_statusView];
   
   NSWindow * window = [self window];
   [window setContentView:m_rootView];
