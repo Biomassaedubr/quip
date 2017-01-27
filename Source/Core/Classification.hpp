dac6aa556f7738953a3d4866e973b2f8e4547756
Josh Petrie <josh@joshpetrie.net>
2017-01-26 19:01:00 -0800

    Clean up classification.

--- Source/Core/Classification.hpp
+++ Source/Core/Classification.hpp
@@ -6,15 +6,7 @@ namespace quip {
   struct Document;
   struct Location;
   struct Selection;
-  
-  enum ClassificationFlags {
-    This,
-    Next,
-    Prior,
-  };
-  
-  typedef std::function<Selection (const Document &, const Location &, ClassificationFlags)> ClassificationFunction;
-  
+
   Selection selectThisWord (const Document & document, const Selection & basis);
   Selection selectNextWord (const Document & document, const Selection & basis);
   Selection selectPriorWord (const Document & document, const Selection & basis);
