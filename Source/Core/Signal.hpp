2eaaa5697bcc87adac7133e34c58c9a947188495
Josh Petrie <josh@joshpetrie.net>
2017-01-27 16:52:20 -0800

    Fix a documentation typo and normalize coding style to new conventions.

--- Source/Core/Signal.hpp
+++ Source/Core/Signal.hpp
@@ -7,31 +7,31 @@ namespace quip {
   
   // A basic multicast delegate. A signal can be connected to listeners of the appropriate
   // signature. When the signal transmits, all connected listeners are called in unspecified
-  // order. Connected listeners are assigned a token when connect which can be used to
+  // order. Connected listeners are assigned a token when connected which can be used to
   // disconnect the listener at a later date. Tokens are unsigned 32-bit integers beginning
   // at one (zero can be used to indicate an invalid or unassigned token).
-  template<typename ReturnType, typename ... ArgumentTypes>
+  template<typename ReturnType, typename... ArgumentTypes>
   struct Signal<ReturnType (ArgumentTypes...)> {
     typedef std::function<ReturnType (ArgumentTypes...)> HandlerType;
     
-    Signal () noexcept
+    Signal() noexcept
     : m_head(nullptr)
     , m_tail(nullptr)
     , m_nextToken(1) {
     }
     
-    ~Signal () {
-      HandlerNode * cursor = m_head;
-      while (cursor != nullptr) {
-        HandlerNode * previous = cursor;
+    ~Signal() {
+      HandlerNode* cursor = m_head;
+      while(cursor != nullptr) {
+        HandlerNode* previous = cursor;
         cursor = cursor->next;
         
         delete previous;
       }
     }
     
-    std::uint32_t connect (const HandlerType & handler) {
-      if (m_head == nullptr) {
+    std::uint32_t connect(const HandlerType& handler) {
+      if(m_head == nullptr) {
         m_head = new HandlerNode(handler, m_nextToken);
         m_tail = m_head;
       } else {
@@ -43,36 +43,34 @@ namespace quip {
       return m_tail->token;
     }
     
-    void disconnect (std::uint32_t token) {
-      HandlerNode * prior = nullptr;
-      HandlerNode * cursor = m_head;
-      while (cursor != nullptr) {
-        if (cursor->token == token) {
-          if (prior != nullptr) {
+    void disconnect(std::uint32_t token) {
+      HandlerNode* prior = nullptr;
+      HandlerNode* cursor = m_head;
+      while(cursor != nullptr) {
+        if(cursor->token == token) {
+          if(prior != nullptr) {
             prior->next = cursor->next;
-          }
-          else {
+          } else {
             m_head = cursor->next;
           }
           
-          if (cursor == m_tail) {
+          if(cursor == m_tail) {
             m_tail = prior;
           }
           
           delete cursor;
           return;
-        }
-        else {
+        } else {
           prior = cursor;
           cursor = cursor->next;
         }
       }
     }
     
-    ReturnType transmit (ArgumentTypes ... arguments) {
-      HandlerNode * cursor = m_head;
-      while (cursor != nullptr) {
-        if (cursor == m_tail) {
+    ReturnType transmit(ArgumentTypes... arguments) {
+      HandlerNode* cursor = m_head;
+      while(cursor != nullptr) {
+        if(cursor == m_tail) {
           return cursor->handler(std::forward<ArgumentTypes>(arguments)...);
         }
         
@@ -85,7 +83,7 @@ namespace quip {
     
   private:
     struct HandlerNode {
-      HandlerNode (const HandlerType & handler, std::uint32_t token) noexcept
+      HandlerNode(const HandlerType& handler, std::uint32_t token) noexcept
       : handler(handler)
       , token(token)
       , next(nullptr) {
@@ -93,11 +91,11 @@ namespace quip {
       
       HandlerType handler;
       std::uint32_t token;
-      HandlerNode * next;
+      HandlerNode* next;
     };
     
-    HandlerNode * m_head;
-    HandlerNode * m_tail;
+    HandlerNode* m_head;
+    HandlerNode* m_tail;
     std::uint32_t m_nextToken;
   };
 }
