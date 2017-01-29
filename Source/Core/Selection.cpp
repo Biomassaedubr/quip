998b43a92fe5711d1af0b2a6fe1f9ee38186fb8b
Josh Petrie <josh@joshpetrie.net>
2017-01-29 15:05:49 -0800

    Remove unused functions.

--- Source/Core/Selection.cpp
+++ Source/Core/Selection.cpp
@@ -3,76 +3,66 @@
 #include <algorithm>
 
 namespace quip {
-  Selection::Selection (const Location & location)
+  Selection::Selection(const Location& location)
   : m_origin(location)
   , m_extent(location) {
   }
   
-  Selection::Selection (const Location & origin, const Location & extent)
+  Selection::Selection(const Location& origin, const Location& extent)
   : m_origin(origin)
   , m_extent(extent) {
     normalize();
   }
   
-  Selection::Selection (const Selection & other)
+  Selection::Selection(const Selection& other)
   : m_origin(other.m_origin)
   , m_extent(other.m_extent) {
   }
   
-  Selection::Selection (Selection && other)
+  Selection::Selection(Selection&& other)
   : m_origin(other.m_origin)
   , m_extent(other.m_extent) {
   }
   
-  Selection & Selection::operator= (const Selection & other) {
+  Selection& Selection::operator=(const Selection& other) {
     m_origin = other.m_origin;
     m_extent = other.m_extent;
     return *this;
   }
   
-  Selection & Selection::operator= (Selection && other) {
+  Selection& Selection::operator=(Selection&& other) {
     m_origin = other.m_origin;
     m_extent = other.m_extent;
     return *this;
   }
 
-  const Location & Selection::origin () const {
+  const Location& Selection::origin() const {
     return m_origin;
   }
   
-  const Location & Selection::extent () const {
+  const Location& Selection::extent() const {
     return m_extent;
   }
 
-  void Selection::setOrigin (const Location & location) {
-    m_origin = location;
-    normalize();
-  }
-  
-  void Selection::setExtent (const Location & location) {
-    m_extent = location;
-    normalize();
-  }
-  
-  std::uint64_t Selection::height () const {
+  std::uint64_t Selection::height() const {
     return m_extent.row() - m_origin.row() + 1;
   }
   
-  void Selection::normalize () {
-    if (m_origin > m_extent) {
+  void Selection::normalize() {
+    if(m_origin > m_extent) {
       std::swap(m_origin, m_extent);
     }
   }
   
-  bool operator== (const Selection & left, const Selection & right) {
-    return left.origin() == right.origin() && left.extent() == right.extent();
+  bool operator==(const Selection& left, const Selection& right) {
+    return left.origin() == right.origin()&& left.extent() == right.extent();
   }
   
-  bool operator!= (const Selection & left, const Selection & right) {
+  bool operator!=(const Selection& left, const Selection& right) {
     return !(left == right);
   }
   
-  void swap (Selection & left, Selection & right) {
+  void swap(Selection& left, Selection& right) {
     using std::swap;
     swap(left.m_origin, right.m_origin);
     swap(left.m_extent, right.m_extent);
--- Source/Core/Selection.hpp
+++ Source/Core/Selection.hpp
@@ -10,31 +10,28 @@ namespace quip {
   // A selection is defined by an origin location and an extent location. If they
   // are not equal, the origin is always earlier in the document than the extent.
   struct Selection {
-    explicit Selection (const Location & location);
-    Selection (const Location & origin, const Location & extent);
-    Selection (const Selection & other);
-    Selection (Selection && other);
+    explicit Selection(const Location& location);
+    Selection(const Location& origin, const Location& extent);
+    Selection(const Selection& other);
+    Selection(Selection&& other);
     
-    Selection & operator= (const Selection & other);
-    Selection & operator= (Selection && other);
+    Selection& operator=(const Selection& other);
+    Selection& operator=(Selection&& other);
     
-    const Location & origin () const;
-    const Location & extent () const;
+    const Location& origin() const;
+    const Location& extent() const;
     
-    void setOrigin (const Location & location);
-    void setExtent (const Location & location);
-    
-    std::uint64_t height () const;
+    std::uint64_t height() const;
 
-    friend void swap (Selection & left, Selection & right);
+    friend void swap(Selection& left, Selection& right);
     
   private:
     Location m_origin;
     Location m_extent;
     
-    void normalize ();
+    void normalize();
   };
   
-  bool operator== (const Selection & left, const Selection & right);
-  bool operator!= (const Selection & left, const Selection & right);
+  bool operator==(const Selection& left, const Selection& right);
+  bool operator!=(const Selection& left, const Selection& right);
 }
