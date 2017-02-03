dfeece1e493973aea4a05e1d1ff4168ea89dc53f
Josh Petrie <josh@joshpetrie.net>
2017-02-02 19:39:02 -0800

    Write a native launcher application to replace the bash script.

---
+++ Launcher/main.m
@@ -0,0 +1,59 @@
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char** argv) {
+  @autoreleasepool {
+    // Check to see if Quip is already running.
+    NSWorkspace* workspace = [NSWorkspace sharedWorkspace];
+    for (NSRunningApplication* application in [workspace runningApplications]) {
+      if ([[application bundleIdentifier] isEqualToString:@"net.joshpetrie.Quip"]) {
+        if (argc > 1) {
+          // Open the specified files.
+          NSString* executable = [[application bundleURL] path];
+          for(int argument = 1; argument < argc; ++argument) {
+            BOOL deactivate = argument == argc - 1;
+            [workspace openFile:[NSString stringWithUTF8String:argv[argument]] withApplication:executable andDeactivate:deactivate];
+          }
+        } else {
+          // Just show the application.
+          [application unhide];
+          [application activateWithOptions:NSApplicationActivateAllWindows];
+        }
+        
+        return 0;
+      }
+    }
+    
+    // If the application isn't running, find it and launch it.
+    NSString* path = [[[NSProcessInfo processInfo] environment] objectForKey:@"QUIP_LOCATION"];
+    if (path == nil) {
+      path = @"/Applications/Quip.app";
+    }
+    
+    if (![[NSFileManager defaultManager] isExecutableFileAtPath:path]) {
+      NSLog(@"The Quip bundle at '%@' is not executable.", path);
+      return -1;
+    }
+    
+    NSBundle* bundle = [NSBundle bundleWithPath:path];
+    if (bundle == nil) {
+      NSLog(@"The Quip bundle at '%@' could not be read.", path);
+      return -1;
+    }
+
+    NSMutableArray* arguments = [[NSMutableArray alloc] initWithCapacity:argc - 1];
+    for(int argument = 1; argument < argc; ++argument) {
+      [arguments addObject:[NSString stringWithUTF8String:argv[argument]]];
+    }
+  
+    NSURL* url = [NSURL fileURLWithPath:path];
+    NSDictionary* configuration = @{NSWorkspaceLaunchConfigurationArguments: arguments};
+    NSError* error = nil;
+    NSRunningApplication* result = [workspace launchApplicationAtURL:url options:NSWorkspaceLaunchDefault configuration:configuration error:&error];
+    if (result == nil) {
+      NSLog(@"The Quip bundle at '%@' could not be launched. %@", path, [error localizedDescription]);
+      return -1;
+    }
+  }
+  
+  return 0;
+}
