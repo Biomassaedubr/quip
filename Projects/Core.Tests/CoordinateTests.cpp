dd9dae110d06cc4efec9b2e22105a1a283215db8
Josh Petrie <josh@joshpetrie.net>
2017-05-07 09:08:32 -0700

    Update style.

--- Projects/Core.Tests/CoordinateTests.cpp
+++ Projects/Core.Tests/CoordinateTests.cpp
@@ -4,14 +4,14 @@
 
 using namespace quip;
 
-TEST_CASE("Coordinates can be default-constructed.", "[CoordinateTests]") {
+TEST_CASE("Default-construct a coordinate.", "Coordinate") {
   Coordinate coordinate;
   
   REQUIRE(coordinate.x == Approx(0.0f));
   REQUIRE(coordinate.y == Approx(0.0f));
 }
 
-TEST_CASE("Coordinates can be constructed from values.", "[CoordinateTests]") {
+TEST_CASE("Construct a coordinate from X and Y values.", "Coordinate") {
   Coordinate coordinate(-10.5f, 91.2f);
   
   REQUIRE(coordinate.x == Approx(-10.5f));
